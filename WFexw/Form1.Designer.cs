454792be134b138826cebe96311e7b7e9d77e428
avale1648 <avale1648@gmail.com>
2022-11-23 21:51:41 +0300

    Добавьте файлы проекта.

---
+++ WFexw/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace WFexw
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.checkButton);
+            this.groupBox1.Location = new System.Drawing.Point(468, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 450);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(6, 19);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(75, 23);
+            this.checkButton.TabIndex = 0;
+            this.checkButton.Text = "Check";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(680, 474);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button checkButton;
+    }
+}
+
---
+++ WFexw/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFexw
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private int hintsNumber = 40;
+        private const int n = 3;
+        private const int cellSize = 50;
+        private int[,] map = new int[n * n, n * n];
+        private Button[,] cells = new Button[n * n, n * n];
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateMap();
+        }
+        public void GenerateMap()
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                    map[i, j] = (i * n + i / n + j) % (n * n) + 1;
+            }
+            for (int i = 0; i < 10; i++)
+                MixMapOptionChoose(random.Next(5));
+            CreateMap();
+            ShowHints();
+        }
+        private void MatrixTransposition()
+        {
+            int[,] mapCopy = new int[n * n, n * n];
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    mapCopy[i, j] = map[i, j];
+                }
+            }
+            map = mapCopy;
+        }
+        private void MixRowsInBlock()
+        {
+            int block = random.Next(n);
+            int row1 = random.Next(n);
+            int col1 = block * n + row1;
+            int row2 = random.Next(n);
+            while (row1 == row2)
+                row2 = random.Next(n);
+            int col2 = block * n + row2;
+            for (int i = 0; i < n * n; i++)
+            {
+                int temp = map[col1, i];
+                map[col1, i] = map[col2, i];
+                map[col2, i] = temp;
+            }
+        }
+        private void MixColumnsInBlock()
+        {
+            int block = random.Next(n);
+            int row1 = random.Next(n);
+            int col1 = block * n + row1;
+            int row2 = random.Next(n);
+            while (row1 == row2)
+                row2 = random.Next(n);
+            int col2 = block * n + row2;
+            for (int i = 0; i < n * n; i++)
+            {
+                int temp = map[i, col1];
+                map[i, col1] = map[i, col2];
+                map[i, col2] = temp;
+            }
+        }
+        private void MixBlocksInRow()
+        {
+            int block1 = random.Next(n);
+            int block2 = random.Next(n);
+            while (block1 == block2)
+                block2 = random.Next(n);
+            block1 *= n;
+            block2 *= n;
+            for (int i = 0; i < n * n; i++)
+            {
+                int k = block2;
+                for (int j = block1; j < block1 + n; j++)
+                {
+                    int temp = map[j, i];
+                    map[j, i] = map[k, i];
+                    map[k, i] = temp;
+                    k++;
+                }
+            }
+        }
+        private void MixBlocksInColumn()
+        {
+            int block1 = random.Next(n);
+            int block2 = random.Next(n);
+            while (block1 == block2)
+                block2 = random.Next(n);
+            block1 *= n;
+            block2 *= n;
+            for (int i = 0; i < n * n; i++)
+            {
+                int k = block2;
+                for (int j = block1; j < block1 + n; j++)
+                {
+                    int temp = map[i, j];
+                    map[i, j] = map[i, k];
+                    map[i, k] = temp;
+                    k++;
+                }
+            }
+        }
+        private void CreateMap()
+        {
+            for (int i = 0; i < n * n; i++)
+            {
+                for (int j = 0; j < n * n; j++)
+                {
+                    cells[i, j] = new Button();
+                    cells[i, j].Size = new Size(cellSize, cellSize);
+                    cells[i, j].Location = new Point(i * cellSize, j * cellSize);
+                    cells[i, j].FlatStyle = FlatStyle.Flat;
+                    cells[i, j].BackColor = ((i / 3) + (j / 3)) % 2 == 0 ? Color.LightBlue : Color.SkyBlue;
+                    cells[i, j].Click += cells_Click;
+                    panel1.Controls.Add(cells[i, j]);
+                }
+            }
+        }
+        private void MixMapOptionChoose(int option)
+        {
+            switch (option)
+            {
+                case 0:
+                    MatrixTransposition();
+                    break;
+                case 1:
+                    MixRowsInBlock();
+                    break;
+                case 2:
+                    MixColumnsInBlock();
+                    break;
+                case 3:
+                    MixBlocksInRow();
+                    break;
+                case 4:
+                    MixBlocksInColumn();
+                    break;
+            }
+        }
+        private void ShowHints()
+        {
+            for(int i = 0; i < hintsNumber; i++)
+            {
+                int x = random.Next(n * n);
+                int y = random.Next(n * n);
+                cells[x, y].Text = map[x, y].ToString();
+                cells[x, y].Enabled = false;
+            }
+        }
+        private void cells_Click(object sender, EventArgs e)
+        {
+            Button pressedCell = sender as Button;
+            string cellText = pressedCell.Text;
+            if(string.IsNullOrEmpty(cellText))
+            {
+                pressedCell.Text = "1";
+            }
+            else
+            {
+                int value = int.Parse(cellText);
+                value++;
+                if (value == 10) value = 1;
+                pressedCell.Text = value.ToString();
+            }
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            int fails = 0;
+            for(int i = 0; i < n*n; i++)
+            {
+                for(int j = 0; j < n*n;j++)
+                {
+                    string cellText = cells[i, j].Text;
+                    if (cellText != map[i, j].ToString())
+                        fails++;
+                }
+            }
+            if(fails == 0)
+                MessageBox.Show("Congradilations! You solved Sudoku without errors.", "Victory", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            else
+                MessageBox.Show($"You lose, sudoku was solved with {fails} errors.", "Lose", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            Close();
+        }
+    }
+}
